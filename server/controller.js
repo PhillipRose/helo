7f169289928428bdc76be7456d39960fa7974142
PhillipRose <philliprosejob@gmail.com>
2020-10-09 16:46:04 -0600

    need to do react 1-4 before finishing this. Used pieces from the full stack review but I am missing too much of the foundational react and axios information to complete this from scratch.

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,4 @@
-
+const bcrypt = require('bcryptjs');
 
 
 
@@ -6,11 +6,50 @@
 module.exports = {
     register: async(req,res) => {
         const {username, password, profileImg} =req.body,
-        db =req.app.get('db')
+            db =req.app.get('db');
+
+
+
+
+        const foundUser =await db.users.check_user({email});
+        if(foundUser[0]){
+            return res.status(400).send('Email already in use');
+        }
+
+        let salt = bcrypt.genSaltSync(10),
+            hash = bcrypt.hashSync(password, salt);
+        
+        const newUser = await db.users.register_user({username, email, hash, profilePicture});
+        req.session.user = newUser[0];
+        res.status(201).send(req.session.user);
     },
     login: async(req, res) => {},
     logout: (req,res) =>{
         req.session.destroy()
         res.sendStatus(200);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
