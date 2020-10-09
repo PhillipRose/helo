ead09bc266eaecd756f2641b0bc151fec9d4d2bb
PhillipRose <philliprosejob@gmail.com>
2020-10-09 16:19:45 -0600

    made it to the axios portion of step 2 so far.

--- server/controller.js
+++ server/controller.js
@@ -2,3 +2,15 @@
 
 
 
+
+module.exports = {
+    register: async(req,res) => {
+        const {username, password, profileImg} =req.body,
+        db =req.app.get('db')
+    },
+    login: async(req, res) => {},
+    logout: (req,res) =>{
+        req.session.destroy()
+        res.sendStatus(200);
+    }
+}
--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -2,13 +2,41 @@ import React, {Component} from 'react'
 
 
 export default class Auth extends Component {
+constructor(props){
+    super()
+    this.state ={
+        username: '',
+        password: '',
+        profileImg: ''
+    }
+
+
+}
+
+handleInput = (val) => {
+    this.setState({username: val})
+}
+handlePassword =(val) => {
+    this.setState({password: val})
+}
+
+
 
 
     render(){
         return(
-            <p>
-                Auth Component
-            </p>
+            // Auth Component
+        <div>
+            <div className='Auth-inputs'>
+                <input value = {this.state.username} placeholder='Username' onChange ={(e) => this.handleInput(e.target.value)}/>
+
+                <input value = {this.state.password} placeholder ='Password' onChange ={(e) => this.handlePassword(e.target.value)}/>
+            </div>
+            <div className ='Auth-buttons'>
+                <button  className ='Auth-login'>Login</button>
+                <button className ='Auth-register'>Register</button>
+            </div>
+        </div>
         )
     }
 }
