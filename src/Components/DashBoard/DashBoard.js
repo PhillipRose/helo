8c1436f5c81c25848ca637106ec80a97d210bd62
PhillipRose <philliprosejob@gmail.com>
2020-10-09 10:06:42 -0600

    setup done

---
+++ src/Components/DashBoard/DashBoard.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+
+export default class DashBoard extends Component {
+
+
+    render(){
+        return(
+            <p>
+                DashBoard Component
+            </p>
+        )
+    }
+}
