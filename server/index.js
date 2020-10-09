d2bed53b8971baeedc822b363566d4db349546ee
PhillipRose <philliprosejob@gmail.com>
2020-10-09 10:13:57 -0600

    server set up

---
+++ server/index.js
@@ -0,0 +1,5 @@
+const ctrl = require('./controller');
+const express = require('express');
+const app = express();
+app.use(express.json())
+app.listen(4000, ()=> console.log('server is running on 4000'))
